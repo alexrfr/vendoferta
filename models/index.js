e75c906d25c75aff2212ce4d7e84eb6595d724b3
Alexander Rodriguez <alexrfr@gmail.com>
2014-03-27 16:10:37 +0100

    some text

--- models/index.js
+++ models/index.js
@@ -7,7 +7,8 @@ if (!global.hasOwnProperty('db')) {
 	var uristring =
 	process.env.MONGOLAB_URI ||
 	process.env.MONGOHQ_URL ||
-	'mongodb://localhost/' + dbName;
+	'mongodb://vendofertauser:159A753bcd@ds031628.mongolab.com:31628/heroku_app23353677' + dbName;
+	//'mongodb://localhost/' + dbName;
 
 	// Makes connection asynchronously.  Mongoose will queue up database
 	// operations and release them when the connection is complete.
