f4aeec91120340db192bfc4e2d7a949e8cd9f787
Alexander Rodriguez <alexrfr@gmail.com>
2014-03-31 15:41:43 +0200

    bbdd

--- models/index.js
+++ models/index.js
@@ -5,8 +5,8 @@ if (!global.hasOwnProperty('db')) {
 	var dbName = 'vendoferta'
 
 	var uristring =
-	process.env.MONGOLAB_URI ||
-	process.env.MONGOHQ_URL ||
+	//process.env.MONGOLAB_URI ||
+	//process.env.MONGOHQ_URL ||
 	'mongodb://heroku_app23353677:akulk5943aorm4rkn2dhbmq1vo@ds031628.mongolab.com:31628/heroku_app23353677';
 	//'mongodb://localhost/' + dbName;
 
